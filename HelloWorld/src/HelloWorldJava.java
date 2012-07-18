fbe4cc77bd1a402b74cd649315a0c364b11a6150
hase502 <hase502@gmail.com>
2012-07-18 17:30:35 +0900

    初期登録

---
+++ HelloWorld/src/HelloWorldJava.java
@@ -0,0 +1,12 @@
+
+public class HelloWorldJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("HelloWorldJava");
+	}
+
+}
