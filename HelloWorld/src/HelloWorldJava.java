b1ad8f9c79d6e7f661df3f9461192503f05b328c
hase5021 <hase502@gmail.com>
2015-10-04 15:34:34 +0900

    サンプルコミット

--- HelloWorld/src/HelloWorldJava.java
+++ HelloWorld/src/HelloWorldJava.java
@@ -6,7 +6,7 @@ public class HelloWorldJava {
 	 */
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("HelloWorldJava");
+		System.out.println("HelloWorldJava!!!");
 	}
 
 }
